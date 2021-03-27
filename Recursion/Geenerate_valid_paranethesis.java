1f0299091859827bb00fb6eb21d986f385481733
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2021-03-28 01:35:27 +0530

    Create Geenerate_valid_paranethesis.java

---
+++ Recursion/Geenerate_valid_paranethesis.java
@@ -0,0 +1,50 @@
+package Recurion;
+
+import java.util.ArrayList;
+
+public class Practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n = 3;
+		// Print_valid_parenthesis
+
+		printpar(0, 0, n, n, "");
+		ArrayList<String> res1 = new ArrayList<String>();
+
+		getpar(0, 0, 3, 3, "", res1);
+		System.out.println(res1);
+
+	}
+
+	private static void getpar(int i, int j, int open, int close, String ans, ArrayList<String> res1) {
+		if (i == open && j == close) {
+			System.out.println(ans);
+			res1.add(ans);
+			return;
+		}
+		if (j > i) {
+			return;
+		}
+		if (i > open || j > close) {
+			return;
+		}
+		printpar(i + 1, j, open, close, ans + "(");
+		printpar(i, j + 1, open, close, ans + ")");
+
+	}
+
+	private static void printpar(int i, int j, int open, int close, String ans) {
+		if (i == open && j == close) {
+			System.out.println(ans);
+			return;
+		}
+		if (j > i) {
+			return;
+		}
+		if (i > open || j > close) {
+			return;
+		}
+		printpar(i + 1, j, open, close, ans + "(");
+		printpar(i, j + 1, open, close, ans + ")");
+	}
