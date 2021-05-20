9a3e09ccf7a474bff7d7c374e09c34f1e3de8908
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2021-05-21 02:18:37 +0530

    Create Reverse_LL_recursievly.java

---
+++ Linked_List/Reverse_LL_recursievly.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+      
+        if (head==null||head.next == null) {
+			
+			return head;
+		}
+		ListNode ret = reverseList(head.next);
+		ListNode current = head;
+        current.next.next = current;
+        current.next= null;
+
+		return ret;
+    }
+}
