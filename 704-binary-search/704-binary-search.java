242ac1d55b95f1aedc71b1a1179f7604109ee4d7
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2022-01-07 23:53:49 +0530

    Time: 0 ms (100.00%), Space: 52 MB (7.15%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,22 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int ans = binary(0,nums.length-1,target,nums);
+
+       int i =0;
+        int j = nums.length-1;
+        int ans = binary(i,j,nums,target);
+        
         return ans;
+        
     }
     
-    public int binary(int left,int right,int target,int[] nums){
-        if(right<left){
+    public static int binary(int low , int high , int [] arr, int target){
+        if(low>high){
             return -1;
         }
-        int mid = (left+right)/2;
-        if(target == nums[mid]){
+    int mid = (low+high)/2;
+        if(arr[mid]==target){
             return mid;
         }
-        else if(nums[mid]>target){
-            return binary(left,mid-1,target,nums);
+        else if(arr[mid]>target){
+            return binary(low , mid-1 , arr, target);
         }
         else{
-            return binary(mid+1,right,target,nums);
+             return binary(mid+1 , high , arr, target);
         }
     }
+    
 }
